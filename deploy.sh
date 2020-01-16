0a7aca54b86c13e71059e46487a6b2cff228e690
Mike Fulton <fultonm@ca.ibm.com>
2020-01-15 18:29:56 -0600

    fix up test/build/deploy for zbrew-eqa #30

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#set -x
+function syntax {
+	echo "Syntax: deploy.sh <directory>"
+	echo " deploy the bill of materials to the specified directory"
+	return 0
+}
+
+if [ $# -ne 1 ]; then
+	echo "Need to provide a directory to deploy to. No parameter given."
+	syntax
+	exit 16
+fi
+if [ ! -d $1 ]; then 
+	echo "Need to specify a directory to deploy to. $1 is not a directory."
+	syntax
+	exit 16
+fi
+
+rm -rf $1/eqae20
+mkdir $1/eqae20
+mkdir $1/eqae20/docs
+mkdir $1/eqae20/docs/C
+mkdir $1/eqae20/docs/C/cat1
+
+names=`cat eqae20.bom`
+code="${names}"
+ 
+for c in ${code}; do
+	cp -p ${c} $1/${c}
+done
+
+exit $? 
